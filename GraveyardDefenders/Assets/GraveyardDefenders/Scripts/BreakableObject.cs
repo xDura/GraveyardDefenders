aa59786f046d8e84bd2db84b77b5a066dbf8320a
xDura <3shappyriding@gmail.com>
2019-10-21 14:50:59 +0200

    skeleton can now break walls

---
+++ GraveyardDefenders/Assets/GraveyardDefenders/Scripts/BreakableObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace XD
+{
+    public class BreakableObject : MonoBehaviour
+    {
+        public float currentHP;
+        public float maxHP;
+        public UnityEvent brokeEvent;
+
+        public bool destroyed = false;
+
+        void Start()
+        {
+            currentHP = maxHP;
+            destroyed = false;
+        }
+
+        public void Hit(float dmg)
+        {
+            currentHP -= dmg;
+            currentHP = Mathf.Clamp(currentHP, 0.0f, maxHP);
+            if (currentHP == 0.0f)
+            {
+                brokeEvent.Invoke();
+                destroyed = true; 
+            }
+        }
+    }
+}
--- GraveyardDefenders/Assets/GraveyardDefenders/Scripts/SkeletonController.cs
+++ GraveyardDefenders/Assets/GraveyardDefenders/Scripts/SkeletonController.cs
@@ -12,9 +12,21 @@ namespace XD
         public NavMeshHit navmesh_hit_helper;
         public RaycastHit hit_helper;
 
+        public float attackDamage = 1.0f;
+        public float attackVelocity = 1.0f;
+        public float lastAttackTime = float.NegativeInfinity;
+        public float TimeSinceLastAttack
+        {
+            get { return Time.timeSinceLevelLoad - lastAttackTime; }
+        }
+
+        BreakableObject currentTarget = default;
+
         void Start()
         {
             if (!agent) GetComponent<NavMeshAgent>();
+
+            agent.autoTraverseOffMeshLink = false;
         }
 
         void Update()
@@ -22,15 +34,33 @@ namespace XD
             Camera Camera = Camera.main;
             Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
             Debug.DrawLine(ray.origin, ray.origin + (ray.direction * maxRayDistance), Color.red);
+
+
             if (Input.GetMouseButtonDown(0))
             {
                 if(Physics.Raycast(ray, out hit_helper, maxRayDistance))
                 {
-                    if (NavMesh.SamplePosition(hit_helper.point, out navmesh_hit_helper, 1.0f, NavMesh.AllAreas))
-                        agent.SetDestination(navmesh_hit_helper.position);
-
+                    if (NavMesh.SamplePosition(hit_helper.point, out navmesh_hit_helper, 1.0f, NavMesh.AllAreas)) { agent.SetDestination(navmesh_hit_helper.position); }
                 }
             }
+
+
+            if (agent.nextOffMeshLinkData.valid)
+            {
+                Debug.Log("Has offmeshLink: " + agent.nextOffMeshLinkData.offMeshLink.name);
+                currentTarget = agent.nextOffMeshLinkData.offMeshLink.gameObject.GetComponent<BreakableObject>();
+            }
+
+            if(currentTarget && !currentTarget.destroyed && TimeSinceLastAttack >= attackVelocity)
+                Attack();
+        }
+
+        void Attack()
+        {
+            Debug.Log(name + " is Attacking: " + currentTarget.name);
+            currentTarget.Hit(attackDamage);
+            if (currentTarget.destroyed) agent.CompleteOffMeshLink();
+            lastAttackTime = Time.timeSinceLevelLoad;
         }
     }
 }
