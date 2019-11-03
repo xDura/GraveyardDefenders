2b93f51733360075799b758619c086b6a0bce518
xDura <3shappyriding@gmail.com>
2019-11-03 18:08:00 +0100

    count of the full days that we have survived

--- GraveyardDefenders/Assets/GraveyardDefenders/Scripts/DayNightCycle.cs
+++ GraveyardDefenders/Assets/GraveyardDefenders/Scripts/DayNightCycle.cs
@@ -36,6 +36,7 @@ namespace XD
         {
             get { return (Time.timeSinceLevelLoad - lastPhaseStartTime) > cycleTime; }
         }
+        public int daysSurvived = 0;
 
         public float CycleRemainingTimeNormalized
         {
@@ -44,6 +45,7 @@ namespace XD
 
         void Start()
         {
+            daysSurvived = 0;
             lastPhaseStartTime = Time.timeSinceLevelLoad;
         }
 
@@ -56,6 +58,7 @@ namespace XD
         public void DoTransitionCycle()
         {
             DAY_NIGHT_PHASE nextPhase = (DAY_NIGHT_PHASE)((int)(currentPhase + 1) % dayNightCycleLightAttributes.Count);
+            if (nextPhase == DAY_NIGHT_PHASE.NIGHT) daysSurvived++;
             Debug.LogFormat($"DayNightCycle: Transition from {currentPhase.ToString()} to: {nextPhase.ToString()}");
             DayNightCycleLightAttributes nextPhaseAttribs = dayNightCycleLightAttributes[(int)nextPhase];
             directionalLight.DOIntensity(nextPhaseAttribs.intensity, transitionTime);
