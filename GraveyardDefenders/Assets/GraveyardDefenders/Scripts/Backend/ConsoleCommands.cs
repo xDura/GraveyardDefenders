97ecaa1badff36b9cc85b399e5aef2489b9362ed
xDura <dura.xavi@gmail.com>
2019-12-11 06:47:51 +0100

    Console commands + pausemenu

---
+++ GraveyardDefenders/Assets/GraveyardDefenders/Scripts/Backend/ConsoleCommands.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using CommandTerminal;
+
+namespace XD
+{
+    public static class ConsoleCommands
+    {
+        [RegisterCommand("TimeScale", Help = "Set time scale", MaxArgCount = 1, MinArgCount = 1)]
+        static void SetTimeScale(CommandArg[] args)
+        {
+            Time.timeScale = args[0].Float;
+        }
+    }   
+}
---
+++ GraveyardDefenders/Assets/GraveyardDefenders/Scripts/Gameplay/UIPauseMenu.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace XD
+{
+    public class UIPauseMenu : MonoBehaviour
+    {
+        public bool isOpen;
+        public Canvas canvas;
+
+        public void Awake()
+        {
+            Close();
+        }
+
+        private void OnDestroy()
+        {
+            Close();
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (isOpen) Close();
+                else Open();
+            }
+        }
+
+        public void Open()
+        {
+            canvas.enabled = true;
+            isOpen = true;
+            Time.timeScale = 0.0f;
+        }
+
+        public void Close()
+        {
+            canvas.enabled = false;
+            isOpen = false;
+            Time.timeScale = 1.0f;
+        }
+
+        public void RestartLevel()
+        {
+            SceneManager.LoadScene("Level_01");
+        }
+
+        public void Surrender()
+        {
+            Close();
+            FindObjectOfType<Objective>().Fail();
+        }
+
+        //Unify this with UIMainMenu
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }   
+}
